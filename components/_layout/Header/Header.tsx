fe7c680faf68d737361152bec850c6a8fd6305d8
Razmik <razmik.unanyan@mail.ru>
2025-01-23 08:43:12 -0600

    added app links

--- components/_layout/Header/Header.tsx
+++ components/_layout/Header/Header.tsx
@@ -1,6 +1,10 @@
 'use client';
 
 import React, { FC } from 'react';
+import {
+    IconBrandGooglePlay,
+    IconBrandAppstore,
+} from '@tabler/icons-react';
 import styles from './header.module.scss';
 import { ALink } from '@/components';
 import { CATALOGS } from '@/constants';
@@ -13,28 +17,42 @@ export const Header: FC<IHeaderProps> = ({ ...props }) => {
   const { isSticky } = useSticky(100);
 
   return (
-    <div className={styles.container} {...props}>
-      {!isSticky && (
-        <div className={styles.links}>
-          <HeaderLinks />
-        </div>
-      )}
-      <div className={styles.searchBar}>
-        <SearchBar />
+      <div className={styles.container} {...props}>
+          <div className={styles.mobileLink}>
+              <ALink
+                variant="secondary"
+                label="Appstore"
+                leftSection={<IconBrandAppstore />}
+                href="#"
+              />
+              <ALink
+                variant="secondary"
+                label="GooglePlay"
+                leftSection={<IconBrandGooglePlay />}
+                href="#"
+              />
+          </div>
+          {!isSticky && (
+              <div className={styles.links}>
+                  <HeaderLinks />
+              </div>
+          )}
+          <div className={styles.searchBar}>
+              <SearchBar />
+          </div>
+          {!isSticky && (
+              <div className={styles.catalog}>
+                  {CATALOGS.map((category, index) => (
+                      <ALink
+                        key={index}
+                        variant="secondary"
+                        label={category.label}
+                        leftSection={category.icon}
+                        href={category.href}
+                      />
+                  ))}
+              </div>
+          )}
       </div>
-      {!isSticky && (
-        <div className={styles.catalog}>
-          {CATALOGS.map((category, index) => (
-            <ALink
-              key={index}
-              variant="secondary"
-              label={category.label}
-              leftSection={category.icon}
-              href={category.href}
-            />
-          ))}
-        </div>
-      )}
-    </div>
   );
 };
