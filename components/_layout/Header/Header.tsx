9f11d1ec920732a1b0f9b09411cfc8f3aedea3d5
Razmik <razmik.unanyan@mail.ru>
2025-01-23 15:49:03 -0600

    update resp

--- components/_layout/Header/Header.tsx
+++ components/_layout/Header/Header.tsx
@@ -1,10 +1,7 @@
 'use client';
 
 import React, { FC } from 'react';
-import {
-    IconBrandGooglePlay,
-    IconBrandAppstore,
-} from '@tabler/icons-react';
+import { IconBrandAppstore, IconBrandGooglePlay } from '@tabler/icons-react';
 import styles from './header.module.scss';
 import { ALink } from '@/components';
 import { CATALOGS } from '@/constants';
@@ -17,42 +14,45 @@ export const Header: FC<IHeaderProps> = ({ ...props }) => {
   const { isSticky } = useSticky(100);
 
   return (
-      <div className={styles.container} {...props}>
-          <div className={styles.mobileLink}>
-              <ALink
-                variant="secondary"
-                label="Appstore"
-                leftSection={<IconBrandAppstore />}
-                href="#"
-              />
-              <ALink
-                variant="secondary"
-                label="GooglePlay"
-                leftSection={<IconBrandGooglePlay />}
-                href="#"
-              />
-          </div>
-          {!isSticky && (
-              <div className={styles.links}>
-                  <HeaderLinks />
-              </div>
-          )}
-          <div className={styles.searchBar}>
-              <SearchBar />
-          </div>
-          {!isSticky && (
-              <div className={styles.catalog}>
-                  {CATALOGS.map((category, index) => (
-                      <ALink
-                        key={index}
-                        variant="secondary"
-                        label={category.label}
-                        leftSection={category.icon}
-                        href={category.href}
-                      />
-                  ))}
-              </div>
-          )}
+    <div className={styles.container} {...props}>
+      <div className={styles.mobileLink}>
+          Download:
+          <ALink
+            className={styles.linkAppWrapper}
+            variant="secondary"
+            label="App Store"
+            leftSection={<IconBrandAppstore />}
+            href="#"
+          />
+          <ALink
+            className={styles.linkAppWrapper}
+            variant="secondary"
+            label="Google Play"
+            leftSection={<IconBrandGooglePlay />}
+            href="#"
+          />
       </div>
+      {!isSticky && (
+        <div className={styles.links}>
+          <HeaderLinks />
+        </div>
+      )}
+      <div className={styles.searchBar}>
+        <SearchBar />
+      </div>
+      {!isSticky && (
+        <div className={styles.catalog}>
+          {CATALOGS.map((category, index) => (
+            <ALink
+              key={index}
+              variant="secondary"
+              label={category.label}
+              leftSection={category.icon}
+              href={category.href}
+            />
+          ))}
+        </div>
+      )}
+    </div>
   );
 };
